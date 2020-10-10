1f42df28e22d6d1b50fd4a8a085f1ee5b90e245a
MiguelF <miguelfelix@campus.ul.pt>
2020-10-10 21:01:48 +0100

    add new

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+import simplegraphics.src.org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle();
+
+        Rectangle rectangle1 = new Rectangle();
+        rectangle.draw();
+
+        }
 }
