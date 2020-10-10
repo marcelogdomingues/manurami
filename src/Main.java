7bfc475718e16d53d9891e038f441a38800e8c85
raquelceirao <raquel.ceirao@gmail.com>
2020-10-10 20:48:06 +0100

    Added lib

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
 public class Main {
     public static void main(String[] args) {
-        ;    }
+
+        Rectangle rectangle = new Rectangle();
+
+        rectangle.draw();
+
+        }
 }
