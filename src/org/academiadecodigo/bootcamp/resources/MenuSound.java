8e1fccb797eccd9e27a2942beb6403fcc152e667
marcelogdomingues <marcelo.domingues.ul@gmail.com>
2020-10-12 18:26:09 +0100

    > Reduce and Increased Sound

--- src/org/academiadecodigo/bootcamp/resources/MenuSound.java
+++ src/org/academiadecodigo/bootcamp/resources/MenuSound.java
@@ -2,6 +2,7 @@ package org.academiadecodigo.bootcamp.resources;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import java.io.File;
 
 public class MenuSound {
@@ -12,6 +13,8 @@ public class MenuSound {
 
                 Clip clip = AudioSystem.getClip();
                 clip.open(AudioSystem.getAudioInputStream(Sound));
+                FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                gainControl.setValue(10.0f); // Aumenta 10 decibeis
                 clip.start();
 
             } catch (Exception e) {
--- src/org/academiadecodigo/bootcamp/resources/Sound.java
+++ src/org/academiadecodigo/bootcamp/resources/Sound.java
@@ -2,6 +2,7 @@ package org.academiadecodigo.bootcamp.resources;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import java.io.File;
 
 public class Sound {
@@ -12,6 +13,8 @@ public class Sound {
 
             Clip clip = AudioSystem.getClip();
             clip.open(AudioSystem.getAudioInputStream(Sound));
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(-20.0f); //Reduz 20 decibeis
             clip.start();
 
         } catch (Exception e) {
