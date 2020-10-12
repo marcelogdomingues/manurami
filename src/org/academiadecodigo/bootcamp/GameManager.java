59c5323125c826406b72b769537554f90974bfef
marcelogdomingues <marcelo.domingues.ul@gmail.com>
2020-10-12 23:26:46 +0100

    Enter Key added to Menu ( Using External Lib ) Space key works the same, now is ENTER and SPACE keys.

--- src/org/academiadecodigo/bootcamp/GameManager.java
+++ src/org/academiadecodigo/bootcamp/GameManager.java
@@ -8,6 +8,8 @@ import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
+import java.awt.event.KeyEvent;
 import java.io.File;
 
 public class GameManager implements KeyboardHandler {
@@ -17,7 +19,7 @@ public class GameManager implements KeyboardHandler {
     private MenuOption selectedMenuOption;
     private boolean menuLocked;
 
-    public void start(int width, int height, int xMargin, int yMargin, Color backgroundColor){
+    public void start(int width, int height, int xMargin, int yMargin, Color backgroundColor) {
 
         selectedMenuOption = MenuOption.values()[0];
 
@@ -61,6 +63,14 @@ public class GameManager implements KeyboardHandler {
             keyboard.addEventListener(spacePressed);
         } //Listening to SPACE key
 
+        {
+            keyboard = new Keyboard(this);
+            KeyboardEvent enterPressed = new KeyboardEvent();
+            enterPressed.setKey(KeyEvent.VK_ENTER);
+            enterPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+            keyboard.addEventListener(enterPressed);
+        }
+
         gameLayout = new GameLayout(width, height, xMargin, yMargin, backgroundColor);
         gameLayout.drawBackground();
         gameLayout.drawMenu();
@@ -78,54 +88,76 @@ public class GameManager implements KeyboardHandler {
         File soundFile = new File("resources/sounds/btn_sound_menu.wav");
         menuSound.playSound(soundFile);
 
-        switch(keyboardEvent.getKey()){
-            case(KeyboardEvent.KEY_W):
-                if(!menuLocked){
-                    if(selectedMenuOption == MenuOption.values()[0]){
+        switch (keyboardEvent.getKey()) {
+            case (KeyboardEvent.KEY_W):
+                if (!menuLocked) {
+                    if (selectedMenuOption == MenuOption.values()[0]) {
                         gameLayout.getMarker().translate(0, 50 * (MenuOption.values().length - 1));
                         selectedMenuOption = MenuOption.values()[MenuOption.values().length - 1];
-                    }else{
+                    } else {
                         gameLayout.getMarker().translate(0, -50);
                         selectedMenuOption = MenuOption.values()[MenuOption.valueOf(selectedMenuOption.toString()).ordinal() - 1];
                     }
                 }
                 break;
-            case(KeyboardEvent.KEY_A):
+
+            case (KeyboardEvent.KEY_A):
                 //nada no menu
                 break;
-            case(KeyboardEvent.KEY_S):
-                if(!menuLocked){
-                    if(selectedMenuOption == MenuOption.values()[MenuOption.values().length - 1]){
+
+            case (KeyboardEvent.KEY_S):
+                if (!menuLocked) {
+                    if (selectedMenuOption == MenuOption.values()[MenuOption.values().length - 1]) {
                         gameLayout.getMarker().translate(0, -50 * (MenuOption.values().length - 1));
                         selectedMenuOption = MenuOption.values()[0];
-                    }else{
+                    } else {
                         gameLayout.getMarker().translate(0, 50);
                         selectedMenuOption = MenuOption.values()[MenuOption.valueOf(selectedMenuOption.toString()).ordinal() + 1];
                     }
                 }
                 break;
-            case(KeyboardEvent.KEY_D):
+
+            case (KeyboardEvent.KEY_D):
                 //nada no menu
                 break;
-            case(KeyboardEvent.KEY_SPACE):
-                if(!menuLocked){
-                    if(selectedMenuOption == MenuOption.EXIT){
+
+            case (KeyboardEvent.KEY_SPACE):
+                if (!menuLocked) {
+                    if (selectedMenuOption == MenuOption.EXIT) {
                         System.exit(0);
-                    }else{
+                    } else {
                         gameLayout.closeMenu();
                         gameLayout.drawInstructions();
                         menuLocked = true;
                     }
-                }else{
+                } else {
                     gameLayout.closeInstructions();
                     gameLayout.drawMenu();
                     gameLayout.getMarker().translate(0, 50 * (MenuOption.valueOf(selectedMenuOption.toString()).ordinal()));
                     menuLocked = false;
                 }
+
+            case (KeyboardEvent.KEY_SPACE):
+                if (!menuLocked) {
+                    if (selectedMenuOption == MenuOption.EXIT) {
+                        System.exit(0);
+                    } else {
+                        gameLayout.closeMenu();
+                        gameLayout.drawInstructions();
+                        menuLocked = true;
+                    }
+                } else {
+                    gameLayout.closeInstructions();
+                    gameLayout.drawMenu();
+                    gameLayout.getMarker().translate(0, 50 * (MenuOption.valueOf(selectedMenuOption.toString()).ordinal()));
+                    menuLocked = false;
+                }
+
         }
 
     }
 
     @Override
-    public void keyReleased(KeyboardEvent keyboardEvent) {}
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+    }
 }
